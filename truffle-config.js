9f43d7d3b627610ab3fd0d092d3119f20ee8611a
do6ryak <ilgiz1996@mail.ru>
2018-06-12 15:11:05 +0300

    init commit

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+     development: {
+       host: "localhost",
+       port: 8545,
+       network_id: "*", // Match any network id
+       gasPrice: 1000000000
+     },
+     ropsten:  {
+       network_id: 3,
+       host: "localhost",
+       port:  8545,
+       gas:  4700035
+     }
+   },
+    rpc: {
+      host: 'localhost',
+      post:8080
+    }
+};
